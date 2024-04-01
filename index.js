c4655721defca8d8774e2f76ccb5e9dc575157c9
Eshwar Pitchiah <55962514+EPitchiah@users.noreply.github.com>
2024-04-01 23:23:21 +0100

    Api Update

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const fs = require('fs');
+const leaderboardData = JSON.parse(fs.readFileSync('./leaderboard.json', 'utf-8'));
+const loginData = JSON.parse(fs.readFileSync('./login.json', 'utf-8'));
+const childData = JSON.parse(fs.readFileSync('./child-accounts.json', 'utf-8'));
+const courseData = JSON.parse(fs.readFileSync('./courses.json', 'utf-8'));
+const parentData = JSON.parse(fs.readFileSync('./parent-account.json', 'utf-8'));
+
+
+
+
+app.listen(PORT, () => {
+    console.log('server running');
+})
+
+app.get('/leader', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(leaderboardData);
+})
+
+app.get('/child', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(childData);
+})
+
+app.get('/course', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(courseData);
+})
+
+app.get('/login', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(loginData);
+})
+
+app.get('/parent', (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.send(parentData);
+})
