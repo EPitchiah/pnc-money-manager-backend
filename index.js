06a5741a522151e797abeae76b20f47789b38742
Eshwar Pitchiah <55962514+EPitchiah@users.noreply.github.com>
2024-04-02 00:31:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 const leaderboardData = JSON.parse(fs.readFileSync('./leaderboard.json', 'utf-8'));
 const loginData = JSON.parse(fs.readFileSync('./login.json', 'utf-8'));
